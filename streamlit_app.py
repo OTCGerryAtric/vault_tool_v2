40e8d71a592f8b57eb7c3c814ee1aa882c992d7f
OTCGerryAtric <gerrypidgeon71@gmail.com>
2023-06-11 20:14:43 +0100

    First Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,54 +12,29 @@ url = f"{authorization_url}?client_id={client_id}&response_type=code"
 st.markdown(f'[Authorize your Bungie Account]({url})')
 webbrowser.open(url)
 
-# Ask the user to enter the access token
-access_token = st.text_input('Please enter your access token')
-st.write(access_token)
+# Ask the user to enter the authorization code
+authorization_code = st.text_input('Please enter the authorization code')
+st.write(authorization_code)
 
 # Endpoint for getting the access token
 token_url = 'https://www.bungie.net/platform/app/oauth/token/'
 
 # The data to send with the request
-data = {'grant_type': 'authorization_code', 'client_id': client_id, 'code': access_token,}
+data = {'grant_type': 'authorization_code', 'client_id': client_id, 'code': authorization_code}
 
 # Headers for the request
-headers = {'X-API-Key': api_key,'Authorization': f"Bearer {access_token}"}
+headers = {'X-API-Key': api_key}
 
 # Send the request
 response = requests.post(token_url, headers=headers, data=data)
-st.write(response)
 
-#
-# response_data = response.json()
-# membership_id = response_data['membership_id']
-# st.write(membership_id)
-#
-# # # The headers for the request
-# # headers = {'X-API-Key': api_key,}
-# #
-# # response = requests.get('https://www.bungie.net/Platform/User/GetMembershipsForCurrentUser/', headers=headers)
-# #
-# # if response.status_code == 200:
-# #     response_data = response.json()
-# #     destiny_membership_id = response_data['Response']['destinyMemberships'][0]['membershipId']
-# #     st.write(f"Destiny Membership ID: {destiny_membership_id}")
-# # else:
-# #     st.write(f"Could not get Destiny Membership ID, response status code: {response.status_code}")
-# #
-# #
-# #
-# # # Check if the request was successful
-# # if response.status_code == 200:
-# #     response_data = response.json()
-# #
-# #     # Print the response_data to debug
-# #     st.write(response_data)
-# #
-# #     # Check if 'membership_id' is in response_data before trying to access it
-# #     if 'membership_id' in response_data:
-# #         membership_id = response_data['membership_id']
-# #         st.write(membership_id)
-# #     else:
-# #         st.write("membership_id not found in response data")
-# # else:
-# #     st.write(f"Could not get access token, response status code: {response.status_code}")
+# If the request was successful, print out the returned data
+if response.status_code == 200:
+    response_data = response.json()
+    access_token = response_data['access_token']
+    membership_id = response_data['membership_id']
+    st.write('Access Token:', access_token)
+    st.write('Membership ID:', membership_id)
+else:
+    st.write('Error:', response.status_code)
+    st.write('Response:', response.text)
