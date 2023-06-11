7f51c1e248172f16e15af72118b1fc7a49178f8e
OTCGerryAtric <gerrypidgeon71@gmail.com>
2023-06-11 18:41:58 +0100

    First Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,10 @@ url = f"{authorization_url}?client_id={client_id}&response_type=code"
 
 st.markdown(f'[Authorize your Bungie Account]({url})')
 webbrowser.open(url)
+
+response = requests.get('https://www.bungie.net/Platform/Destiny2/PLATFORM/Profile/DESTINY_MEMBERSHIP_ID/',
+                            headers=headers)
+    if response.status_code == 200:
+        st.write(response.json())
+    else:
+        st.write(f"Request failed with status code {response.status_code}")
