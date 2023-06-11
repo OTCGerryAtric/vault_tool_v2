eb8762f1abab53f37c3f866d5d65f90cee8e2606
OTCGerryAtric <gerrypidgeon71@gmail.com>
2023-06-11 20:02:48 +0100

    First Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,13 @@ import streamlit as st
 import requests
 import webbrowser
 
+# Setting up Variables
 api_key = 'd9194342565e4bf0b8238751543ecb0b'
 client_id = '44582'
 authorization_url = 'https://www.bungie.net/en/OAuth/Authorize'
-url = f"{authorization_url}?client_id={client_id}&response_type=code"
 
+# Authorise Bungie Account
+url = f"{authorization_url}?client_id={client_id}&response_type=code"
 st.markdown(f'[Authorize your Bungie Account]({url})')
 webbrowser.open(url)
 
@@ -16,33 +18,30 @@ webbrowser.open(url)
 access_token = st.text_input('Please enter your access token')
 st.write(access_token)
 
-
 # Endpoint for getting the access token
 token_url = 'https://www.bungie.net/platform/app/oauth/token/'
 
 # The data to send with the request
-data = {
-    'grant_type': 'authorization_code',
-    'client_id': client_id,
-    'code': access_token,
-}
+data = {'grant_type': 'authorization_code', 'client_id': client_id, 'code': access_token,}
 
-# The headers for the request
-headers = {
-    'X-API-Key': api_key,
-}
+# Send the request
+response = requests.post(token_url, headers=headers, data=data)
+membership_id = response_data['membership_id']
+st.write(membership_id)
 
-response = requests.get('https://www.bungie.net/Platform/User/GetMembershipsForCurrentUser/', headers=headers)
-
-if response.status_code == 200:
-    response_data = response.json()
-    destiny_membership_id = response_data['Response']['destinyMemberships'][0]['membershipId']
-    st.write(f"Destiny Membership ID: {destiny_membership_id}")
-else:
-    st.write(f"Could not get Destiny Membership ID, response status code: {response.status_code}")
-
-# # Send the request
-# response = requests.post(token_url, headers=headers, data=data)
+# # The headers for the request
+# headers = {'X-API-Key': api_key,}
+#
+# response = requests.get('https://www.bungie.net/Platform/User/GetMembershipsForCurrentUser/', headers=headers)
+#
+# if response.status_code == 200:
+#     response_data = response.json()
+#     destiny_membership_id = response_data['Response']['destinyMemberships'][0]['membershipId']
+#     st.write(f"Destiny Membership ID: {destiny_membership_id}")
+# else:
+#     st.write(f"Could not get Destiny Membership ID, response status code: {response.status_code}")
+#
+#
 #
 # # Check if the request was successful
 # if response.status_code == 200:
